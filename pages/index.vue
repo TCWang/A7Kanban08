f8d33e77e52f1755d34a7b3188ec71c9feddac34
TC Wang <wangtct@hotmail.com>
2023-10-24 20:21:04 +0800

    map

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <h1>A7新林口-社區網站: A7重劃區 城市探索</h1>
+    <NuxtImg
+      src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/introduction/INT-004.png"
+      alt="A7 map"
+      class="w-full"
+    />
+
+    <div class=" ">
+      A7 重劃區 街道圖
+      <a href="https://www.openstreetmap.org/#map=15/25.0462/121.3848"
+        >OpenStreetmap</a
+      >,
+      <a
+        href="https://poi.zhupiter.com/map843161-%E6%A1%83%E5%9C%92%E5%B8%82%E9%BE%9C%E5%B1%B1%E5%8D%80%E5%85%AC%E6%89%80%E6%A8%82%E5%96%84%E9%87%8C%E8%BE%A6%E5%85%AC%E8%99%95%E5%9C%B0%E5%9C%96.html"
+        >ZhupiterMap</a
+      >
+      <br />
+    </div>
   </div>
 </template>
 
