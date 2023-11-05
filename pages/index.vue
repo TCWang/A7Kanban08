f66ba4ad1e62ceace8293c8957e669f6e45a9bdb
TC Wang <wangtct@hotmail.com>
2023-11-05 16:27:50 +0800

    urban

--- pages/index.vue
+++ pages/index.vue
@@ -17,10 +17,6 @@
       A7 重劃區 街道圖
       <a href="https://www.openstreetmap.org/#map=15/25.0462/121.3848"
         >OpenStreetmap</a
-      >,
-      <a
-        href="https://poi.zhupiter.com/map843161-%E6%A1%83%E5%9C%92%E5%B8%82%E9%BE%9C%E5%B1%B1%E5%8D%80%E5%85%AC%E6%89%80%E6%A8%82%E5%96%84%E9%87%8C%E8%BE%A6%E5%85%AC%E8%99%95%E5%9C%B0%E5%9C%96.html"
-        >ZhupiterMap</a
       >
       <br />
     </div>
